a6195a6b0a7a4362e4615d23f8cc3e3a1a8a8457
Momchil Tomov <tomov90@gmail.com>
2018-11-04 18:54:03 -0500

    gen_data

---
+++ gen_data.m
@@ -0,0 +1,43 @@
+function gen_data(filename, nsubj, nsesh, nblocks, ntrials)
+
+    subj = [];
+    sesh = [];
+    block = [];
+    trial = [];
+    cond = [];
+    cue = [];
+    r1 = [];
+    r2 = [];
+    reward = [];
+    choice = [];
+    for s = 1:nsubj
+        for se = 1:nsesh
+            for b = 1:nblocks
+                for t = 1:ntrials
+                    cnd = mod(b, 2) + 1;
+                    cu = randi([1 2]);
+                    if (cnd == 1 && cu == 1) || (cnd == 2 && cu == 2)
+                        r = [1 0];
+                    else
+                        r = [0 1];
+                    end
+                    ch = randi([1 2]);
+
+                    subj = [subj; s];
+                    sesh = [sesh; se];
+                    block = [block; b];
+                    trial = [trial; t];
+                    cond = [cond; cnd];
+                    cue = [cue; cu];
+                    r1 = [r1; r(1)];
+                    r2 = [r2; r(2)];
+                    choice = [choice; ch];
+                    reward = [reward; r(ch)];
+                end
+            end
+        end
+    end
+
+    T = table(subj, sesh, block, trial, cond, cue, r1, r2, choice, reward);
+    writetable(T, filename);
+
